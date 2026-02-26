d0ac31d4f7eba7835102c40065fff77f9014bbc9
Hector Casablanca <jeremiahedu2019@gmail.com>
2026-02-25 23:00:52 -0700

    These are the alterations I have made to fulfill my requirements. Check with team to see if it is enough.

--- Mission8_Assignment/Controllers/HomeController.cs
+++ Mission8_Assignment/Controllers/HomeController.cs
@@ -1,16 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Mission8_Assignment.Models;
-using System.Diagnostics;
 
 namespace Mission8_Assignment.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly QuadrantContext _context;
+
+        public HomeController(QuadrantContext context)
         {
-            return View();
+            _context = context;
         }
 
+        public IActionResult Index()
+        {
+            var quadrants = _context.Quadrants
+                .Include(q => q.Category)
+                .ToList();
+            return View(quadrants);
+        }
 
+        [HttpGet]
+        public IActionResult addTask()
+        {
+            // Get categories from the database and create a SelectList
+            // The SelectList takes the value field (CategoryId) and the text field (CategoryName)
+            ViewBag.categories = new SelectList(
+                _context.Categories
+                        .OrderBy(c => c.CategoryName)
+                        .ToList(),
+                "CategoryId",       // value field
+                "CategoryName"      // text field
+            );
+
+            // Pass a new quadrant object to the view
+            return View("addTask", new Quadrant());
+        }
     }
 }
